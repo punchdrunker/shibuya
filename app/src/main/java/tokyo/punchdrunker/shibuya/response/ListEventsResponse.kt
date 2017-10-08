a5fe94998bf56d70cfad3fd7fd77d5afd88059e2
punchdrunker <nanao.hhk@gmail.com>
2017-10-08 12:40:50 +0900

    add test cases

--- app/src/main/java/tokyo/punchdrunker/shibuya/response/ListEventsResponse.kt
+++ app/src/main/java/tokyo/punchdrunker/shibuya/response/ListEventsResponse.kt
@@ -6,7 +6,7 @@ import tokyo.punchdrunker.shibuya.entity.ConnpassEvent
  * Created by punchdrunker on 2017/09/30.
  */
 data class ListEventsResponse(
-        val resultsReturned: Int,
-        val resultsAvailable: Int,
-        val resultsStart: Int,
+        val results_returned: Int,
+        val results_available: Int,
+        val results_start: Int,
         val events: List<ConnpassEvent>)
